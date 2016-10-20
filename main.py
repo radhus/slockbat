0804bc43644356bd93c14b84c8cf9edec5da2a91
max@ig4.se <max@ig4.se>
2016-10-20 02:20:52 +0200

    :poop:

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
+import sys
+
 from slackbot.bot import Bot
 
 
 def run():
-    slackbot = Bot()
+    from utils import parse_config
+    config = parse_config('config.ini')
+
+    if 'main' not in config:
+        print("Missing main in config.")
+        sys.exit(0)
+
+    if 'slack' not in config:
+        print("Missing slack in config")
+        sys.exit(0)
+
+    slackbot = Bot(config)
 
     try:
         slackbot.start()
--- slackbot/bot.py
+++ slackbot/bot.py
@@ -1,32 +1,42 @@
-import sys
-import json
 import datetime
+import json
+import sys
+
 import mongoengine
-
 from slacksocket import SlackSocket
-from slackbot.models.message import Message
 
-"""
-Put this in a config file, together with other things such as mongo URI.
-xoxb-84282495654-KHPGRmPs7x1cyB534EuhCZi8
-"""
+from models.message import Message
 
 
 class Bot:
-    def __init__(self):
-        self.s = SlackSocket('xoxb-84282495654-KHPGRmPs7x1cyB534EuhCZi8')
+    def __init__(self, config):
+        self.s = SlackSocket(config['slack'].get('token'))
+
+        # Wrap this in the connect call?
+        self.mongo_db = config['main'].get('mongo_db')
+        self.mongo_host = config['main'].get('mongo_host')
+        self.mongo_port = config['main'].getint('mongo_port')
 
         # Is this a good way to handle mongo connection? Seems weird.
         self.mongo = mongoengine
-        self.mongo.connect('slackbot', host='10.0.1.5', port=27017)
+        self.mongo.connect(self.mongo_db,
+                           host=self.mongo_host,
+                           port=self.mongo_port)
 
     def start(self):
         for event in self.s.events():
             if event.type == 'message':
-                event_json = json.loads(event.json)
-                print(event_json)
-                message = Message(user=event_json['user'], text=event_json['text'], timestamp=datetime.datetime.now())
-                print(message.user)
+                # TODO Pass when message has subtype, eg. message_changed.
+                # Maybe do this in a nicer way.
+                if 'subtype' in event.json:
+                    pass
+                else:
+                    event_json = json.loads(event.json)
+                    message = Message(user=event_json['user'],
+                                      channel=event_json['channel'],
+                                      text=event_json['text'].strip(),
+                                      timestamp=datetime.datetime.utcnow())
+                    message.save()
 
     def quit(self):
         self.s.close()
